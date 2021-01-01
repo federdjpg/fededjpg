6901b4755c4de5323400e1e8b77c73442e842a30
fededjpg <fededj-247@hotmail.com>
2020-12-31 21:56:08 -0600

    start2

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Federico Pérez Gómez - Portafolio</title>
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
+</head>
+<body>
+  <header class="bg-red-500 text-normal text-lg flex flex-wrap justify-end content-center sticky top-0 h-12">
+    <p class="float-left">hola</p>
+    <p class="text-white mr-3 hover:bg-red-700 cursor-pointer"><a href="#portafolio">Portafolio</a></p>
+    <p class="text-white mr-3 hover:bg-red-700 cursor-pointer"><a href="#skills">Skills</a></p>
+    <p class="text-white mr-3 hover:bg-red-700 cursor-pointer">Experience</p>
+  </header>
+
+  <section>
+   <article id="portafolio">
+
+   </article>
+
+   <article id="skills">
+  
+   </article>
+  </section>
+
+  <footer>
+
+  </footer>
+</body>
+</html>
